d7cbe6b7364be458a5152f4f44b75c767f4d0c52
kingsjaymes76@gmail.com <155890307+Jamon745@users.noreply.github.com>
2024-07-08 05:17:59 -0700

    Favorite icons updated

--- src/Components/Pages/Products.jsx
+++ src/Components/Pages/Products.jsx
@@ -11,8 +11,8 @@ const Products = () => {
 
       {/* section for displaying service benefits to our Customers */}
 
-      <div className="w-[1276px] mx-auto bg-[#d9d9d9] h-[275px] rounded-2xl flex items-center justify-center text-center mt-10">
-        <div className="flex flex-col p-1 border-r-2 border-gray-500 px-4 w-[225px]">
+      <div className="container mx-auto bg-[#d9d9d9] h-[250px] rounded-2xl flex items-center justify-center text-center mt-10">
+        <div className="flex flex-col p-1 border-r-2 border-gray-500 px-4 w-[300px]">
           <img
             className="bg-white rounded-full p-1 self-center"
             src={Vector1}
@@ -22,7 +22,7 @@ const Products = () => {
           <p>Meticulous craftsmanship, true authentic beauty.</p>
         </div>
 
-        <div className="flex flex-col p-1 border-r-2 border-gray-500 px-4 w-[225px]">
+        <div className="flex flex-col p-1 border-r-2 border-gray-500  w-[300px]">
           <img
             className="bg-white rounded-full p-1 self-center"
             src={Vector2}
@@ -32,9 +32,9 @@ const Products = () => {
           <p>Enjoy fast, reliable worldwide shipping.</p>
         </div>
 
-        <div className="flex flex-col p-1 border-r-2 border-gray-500 px-4 w-[225px]">
+        <div className="flex flex-col border-r-2 border-gray-500  w-[300px]">
           <img
-            className="bg-white rounded-full p-1 self-center"
+            className="bg-white rounded-full self-center"
             src={Vector3}
             alt=""
           />
@@ -42,9 +42,9 @@ const Products = () => {
           <p>Experience secure and swift payments.</p>
         </div>
 
-        <div className="flex flex-col w-[225px]">
+        <div className="flex flex-col px-6 w-[300px]">
           <img
-            className="bg-white rounded-full p-1 self-center px-4"
+            className="bg-white rounded-full p-1 self-center"
             src={Vector4}
             alt=""
           />
@@ -56,21 +56,21 @@ const Products = () => {
       {/* Products */}
       <div>
             <h1 className="text-center font-bold text-2xl mt-12 ">Our Products</h1>
-            <ul className="flex items-center gap-6 justify-evenly">
-                  <li>All</li>
-                  <li>Necklace</li>
-                  <li>Earings</li>
-                  <li>Wristwatches</li>
-                  <li>Leg Chains</li>
-                  <li>Bangles</li>
-                  <li>Nose Rings</li>
+            <ul className="flex items-center gap-6 justify-evenly mt-10 h-10">
+                  <li className="bg-primary w-20 rounded-lg px-2 py-2 text-center hover:border-2 hover:border-secondary">All</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Necklace</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Earings</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Wristwatches</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Leg Chains</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Bangles</li>
+                  <li className="hover:border-b-2 hover:border-secondary">Nose Rings</li>
             </ul>
             <div className="grid grid-cols-4">
 
             {ProductData.map((data) => (
                   <div key={data.id} className="mx-auto mt-10">
                         <img src={data.img} width="300px" height="300px" alt="" />
-                        < FaHeart className="text-primary text-xl hover:translate-y-1 bg-white rounded-full p-1 absolute -mt-60 ml-60"/>
+                        < FaHeart className="text-primary outline-transparent text-xl hover:translate-y-1 bg-white rounded-full p-1 absolute -mt-64 ml-60"/>
                         <p className="flex items-center justify-between mt-3">{data.description} <FaBagShopping className="text-primary/50 bg-white rounded-full p-1 text-xl"/></p>
                         <h3 className="font-semibold">{data.price}</h3>
                   </div>
