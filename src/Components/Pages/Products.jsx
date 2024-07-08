0d52923c906bda44bcefa83d1af7f9a32678421a
kingsjaymes76@gmail.com <155890307+Jamon745@users.noreply.github.com>
2024-07-08 06:59:34 -0700

    Steps to Order

--- src/Components/Pages/Products.jsx
+++ src/Components/Pages/Products.jsx
@@ -3,16 +3,17 @@ import Vector2 from "../../assets/Images/fa-solid_shipping-fast.png";
 import Vector3 from "../../assets/Images/subway_tick.png";
 import Vector4 from "../../assets/Images/flowbite_wallet-solid.png";
 import { ProductData } from "./ProductData";
-import { FaBagShopping, FaHeart } from "react-icons/fa6";
+import { FaArrowRightLong, FaBagShopping, FaHeart } from "react-icons/fa6";
+import GuidianImg from "../../assets/Images/Shopping girl using shopping card.png";
+import ReviewImg from "../../assets/Images/Rectangle 23.png";
 
 const Products = () => {
   return (
     <div className="bg-[#ece6d3] h-auto">
-
       {/* section for displaying service benefits to our Customers */}
 
       <div className="container mx-auto bg-[#d9d9d9] h-[250px] rounded-2xl flex items-center justify-center text-center mt-10">
-        <div className="flex flex-col p-1 border-r-2 border-gray-500 px-4 w-[300px]">
+        <div className="flex flex-col p-1 border-r-2 border-gray-500 w-[300px]">
           <img
             className="bg-white rounded-full p-1 self-center"
             src={Vector1}
@@ -55,28 +56,104 @@ const Products = () => {
 
       {/* Products */}
       <div>
-            <h1 className="text-center font-bold text-2xl mt-12 ">Our Products</h1>
-            <ul className="flex items-center gap-6 justify-evenly mt-10 h-10">
-                  <li className="bg-primary w-20 rounded-lg px-2 py-2 text-center hover:border-2 hover:border-secondary">All</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Necklace</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Earings</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Wristwatches</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Leg Chains</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Bangles</li>
-                  <li className="hover:border-b-2 hover:border-secondary">Nose Rings</li>
-            </ul>
-            <div className="grid grid-cols-4">
+        <h1 className="text-center font-bold text-2xl mt-12 ">Our Products</h1>
+        <ul className="flex items-center gap-6 justify-evenly mt-10 h-10">
+          <li className="bg-primary w-20 rounded-lg px-2 py-2 text-center hover:border-2 hover:border-secondary">
+            All
+          </li>
+          <li className="hover:border-b-2 hover:border-secondary">Necklace</li>
+          <li className="hover:border-b-2 hover:border-secondary">Earings</li>
+          <li className="hover:border-b-2 hover:border-secondary">
+            Wristwatches
+          </li>
+          <li className="hover:border-b-2 hover:border-secondary">
+            Leg Chains
+          </li>
+          <li className="hover:border-b-2 hover:border-secondary">Bangles</li>
+          <li className="hover:border-b-2 hover:border-secondary">
+            Nose Rings
+          </li>
+        </ul>
+        <div className="grid grid-cols-4">
+          {ProductData.map((data) => (
+            <div key={data.id} className="mx-auto mt-10">
+              <img src={data.img} width="300px" height="300px" alt="" />
+              <FaHeart className="text-primary outline-transparent text-xl hover:translate-y-1 bg-white rounded-full p-1 absolute -mt-64 ml-60" />
+              <p className="flex items-center justify-between mt-3">
+                {data.description}{" "}
+                <FaBagShopping className="text-primary/50 bg-white rounded-full p-1 text-xl" />
+              </p>
+              <h3 className="font-semibold">{data.price}</h3>
+            </div>
+          ))}
+        </div>
+        {/* How to order section */}
+        <div className=" bg-white py-10">
+          <div className="flex container mx-auto">
+            {/* Steps to Order */}
+            <div>
+              <h1 className="text-center text-4xl uppercase font-semibold">
+                How to Order
+              </h1>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Explore our exquisite range of jewelry pieces
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Click on the desired item to view details
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Click &apos;Add to Cart&apos; to place the item in your shopping
+                cart
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Click &apos;Checkout&apos; to start the payment process
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Provide your shipping address and contact details
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Choose your payment method and enter the required details
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Double-check all order details, including shipping and payment
+                information.
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                Click &apos;Place Order&apos; to complete your purchase.
+              </p>
+              <p className="flex items-center gap-3 mt-4">
+                <FaArrowRightLong className="text-primary " />
+                You will receive an order confirmation email with tracking
+                information.
+              </p>
+            </div>
 
-            {ProductData.map((data) => (
-                  <div key={data.id} className="mx-auto mt-10">
-                        <img src={data.img} width="300px" height="300px" alt="" />
-                        < FaHeart className="text-primary outline-transparent text-xl hover:translate-y-1 bg-white rounded-full p-1 absolute -mt-64 ml-60"/>
-                        <p className="flex items-center justify-between mt-3">{data.description} <FaBagShopping className="text-primary/50 bg-white rounded-full p-1 text-xl"/></p>
-                        <h3 className="font-semibold">{data.price}</h3>
-                  </div>
-            ))}
+            {/* order guidian images */}
+            <div className="mx-auto">
+              <img src={GuidianImg} height="551.18px" width="370px" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
 
+      {/* Customer testimonial */}
+      <div>
+        <div className="container mx-auto flex h-[50vh]">
+          <div className="bg-secondary max-h-48 max-w-40 rounded-2xl mt-10">
+            <img src={ReviewImg} className="mt-10 ml-14 relative" alt="" />
+          </div>
+          <div>
+            <h1>Customer Review</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
